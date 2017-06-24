e599ed14b20781506c6ed344186a5bf5a3a957a3
amnotafraid <amnotafraid@gmail.com>
2017-06-24 11:37:14 -0500

    works - needs cleanup

--- index.js
+++ index.js
@@ -8,21 +8,12 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 
-const versionNumber = parseInt(process.version.substring(1).split('.')[0],10);
-/*
-let a = [];
-if (versionNumber < 5) {
-  a = ['-','+'];
-}
-else {
-  a = ['|','m'];
-}
-*/
 var cnt = 0;
 var iDepth = 0;
 var iLenBefore;
 var iSpacesBefore;
 var sOutput;
+var bEndDependencyBefore;
 
 const cr = '\n';
 
@@ -73,84 +64,68 @@ function endDependency() {
 rl.on('line', function(line) {
     let sLine     = line.toString();
     let aS        = sLine.split(' ');
-    let iLen      = aS.length;
-    let sModule   = aS[iLen - 1];
+    let sModule   = aS[aS.length - 1];
+    let iLen      = Math.floor(sLine.lastIndexOf(sModule) / 2);
     let iSpaces   = 0;
     let sStr      = '';
     let ch        = aS[iLen - 2].slice(0,1);
     let chIndex   = sLine.lastIndexOf(ch);
+    let bEndDependency = false;
+    let bBoth     = false;
+    let bSame     = false;
 
     sOutput       = '';
+
+    if (ch.charCodeAt(0) === 96) {
+      bEndDependency = true;
+    }
+
+    bBoth = bEndDependency && bEndDependencyBefore;
+    bSame = (iLenBefore === iLen);
 
     while (sLine.charCodeAt(chIndex - 1) === 32 && iSpaces < 10) {
       iSpaces++;
       chIndex--;
     }
 
-    sStr += `${sLine} before=${iLenBefore} now=${iLen} ch=${ch} iSpacesBefore=${iSpacesBefore}`;
+//    sStr += `${sLine} before=${iLenBefore} now=${iLen} ch=${ch} iSpacesBefore=${iSpacesBefore} bBoth = ${bBoth}`;
+    sStr += `[${cnt}] - ${sModule} bSame=${bSame} iSpacesBefore=${iSpacesBefore} bBoth = ${bBoth}`;
 
-    /*
-    if (versionNumber < 5) {
-      ch          = aS[iLen - 2].slice(-1);
+  if (true) { //cnt < 40) {
+    if (cnt === 0) {
+      sOutput += begin(sModule);
     }
-    */
-//      sOutput += `Line(${cnt}) | ${aS.length} | ${ch} | ${ch.charCodeAt(0)} | ${aS[aS.length - 1]}\n`;
-  //      let ch = aS[iLen - 2].slice(0, 1);
-  //      if (versionNumber < 5) {
-  //      }
-  //      let sOutput = `Line(${cnt}) | ${aS.length} | ${ch} | ${ch.charCodeAt(0)} | ${aS[aS.length - 1]}\n`;
-        
-    // ok, Einstein.  Use lastIndexOf.  sModule, then second to last
-    // then last.  Then find the spaces.
-
-    
-       /*
-    if (iLen > 2) {
-      let sStr3 = aS[iLen - 3];
-      let iLen3 = sLine.lastIndexOf(sStr3);
-      let sStr2 = aS[iLen - 2];
-      let iLen2 = sLine.lastIndexOf(sStr2);
-      let iLen1 = sLine.lastIndexOf(sModule);
-      let iLenX = aS[iLen - 3].length;
-      let iSpaces = iLen2 - iLen3 + iLenX
-      sStr = `${sModule}(${iLen1})  ${sStr2}(${iLen2})  ${sStr3}(${iLen3}) spaces=${iSpaces}`;
-    }
-   */
-    if (true) { //cnt < 40) {
-      if (cnt === 0) {
-        sOutput += begin(sModule);
+    else {
+      if (iLen === iLenBefore) {
+        sOutput += endModule();
+        sOutput += beginModule(sModule);
       }
-      else {
-//        sOutput += sStr;
-        if (iLen === iLenBefore) {
-          sOutput += endModule();
-          sOutput += beginModule(sModule);
+      else if (iLen > iLenBefore) {
+        sOutput += beginDependency(sModule);
+      }
+      else if (iLen < iLenBefore) {
+        for(let i = iLen; i < iLenBefore; i++) {
+          sOutput += endDependency();
         }
-        else if (iLen > iLenBefore) {
-          sOutput += beginDependency(sModule);
-        }
-        else if (iLen < iLenBefore) {
-          for(let i = 0; i < iSpacesBefore; i += 2) {
-            sOutput += endDependency();
-          }
-          sOutput += endModule();
-          sOutput += beginModule(sModule);
-        }
+        sOutput += endModule();
+        sOutput += beginModule(sModule);
       }
     }
+  }
 
   iLenBefore = iLen;
   iSpacesBefore = iSpaces;
+  bEndDependencyBefore = bEndDependency;
   cnt++;
   process.stdout.write(new Buffer(sOutput));
 })
 .on('close', function() {
-  sOutput = `iSpacesBefore = ${iSpacesBefore} iDepth = ${iDepth}`;
+  sOutput  = '';
   while (iDepth > 0) {
     iDepth -= 2;
     sOutput +=  cr + ' '.repeat(iDepth) + '}'; 
   }
   sOutput += cr;
   process.stdout.write(new Buffer(sOutput));
-//  process.exit(0);
+  process.exit(0);
 });
